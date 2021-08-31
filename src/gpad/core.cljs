a11cf5ab9a3185ee851a98f8df0d07ef01c559d2
Aleksandr Kondratyev <aleksandr.kondratyev@suprematic.net>
2021-09-01 00:52:39 +0500

    add navigation

--- src/gpad/core.cljs
+++ src/gpad/core.cljs
@@ -2,27 +2,18 @@
   (:require [reagent.core :as r]
             [reagent.react-native :as rn]
             [react-native :as rrn]
-            [clojure.string :as str]
-            [gpad.viewer :as viewer]))
+            [re-frame.core :as rf]
+            ["@react-navigation/native" :refer [NavigationContainer]]
+            ["@react-navigation/native-stack" :refer [createNativeStackNavigator]]
 
-(comment
-  (def flat-list (r/adapt-react-class rrn/FlatList)))
+            [clojure.string :as str]
+            [gpad.viewer :as viewer]
+            [gpad.song-list :as song-list]))
+
+(def nav-container (r/adapt-react-class NavigationContainer))
+
 
 (defn log [d] (js/console.log d))
-
-(log "start")
-(log rrn/Separator)
-
-(defn separator []
-  [rn/view {:style {:borderBottomColor "black"
-                    :borderBottomWidth 2}}])
-
-(defn item [item]
-  (let [item (.-item item)]
-    (r/as-element
-      [rn/view
-       [rn/text {:style {:font-size 40}} (.-title item)]
-       [separator]])))
 
 (def lyric
   ["[Asus4]Here's a thought for every man who tries to understand"
@@ -38,13 +29,72 @@
    "[Am][C][D][E]"
    "And [D]after all,[Asus7]"])
 
-(defn hello []
-  [rn/safe-area-view {:style {:flex 1}} #_{:style {:flex 1 :align-items "center" :justify-content "center"}}
+(defn prepare-navigator
+  [navigator screen]
+  (fn [& params]
+    (let [[props children] (if (map? (first params))
+                             [(first params) (second params)]
+                             [{} (first params)])]
+      (into [navigator props]
+            (mapv (fn [props]
+                    [screen (update props :component r/reactify-component)])
+                  children)))))
 
-   [viewer/viewer lyric]
+(defn create-stack-navigator
+  []
+  (let [^js stack (createNativeStackNavigator)]
+    [(r/adapt-react-class (.-Navigator stack))
+     (r/adapt-react-class (.-Screen stack))]))
+
+(defn stack
+  [& params]
+  (let [[navigator screen] (create-stack-navigator)]
+    (prepare-navigator navigator screen)))
+
+(rf/reg-event-fx
+  :navigate-to
+  (fn [{:keys [db]} [_ route]]
+    {:db       db
+     :navigate route}))
+
+(defonce nav-ref-state (atom nil))
+
+(rf/reg-fx
+  :navigate
+  (fn [route]
+    (when-let [nav-ref @nav-ref-state]
+      (.navigate ^js nav-ref route))))
+
+(def test-data
+  [{:title "Masha 21" :on-click (fn []
+                                  (rf/dispatch [:navigate-to :song]))}
+   {:title "Dasha 11"}])
+
+(def list-state (r/atom test-data))
+
+(defn songs-page []
+  [song-list/song-list @list-state])
+
+(defn song-page []
+  [viewer/viewer lyric])
+
+(defn main-page []
+  [rn/safe-area-view {:style {:flex 1}} #_{:style {:flex 1 :align-items "center" :justify-content "center"}}
+   [nav-container {:ref #(reset! nav-ref-state %)}
+    [stack
+     [{:name      :main
+       :component songs-page}
+      {:name      :song
+       :component song-page}]]]
+   #_(case @page-state
+     :main
+     [song-list/song-list @list-state]
+     :song
+     [viewer/viewer lyric])
+
    #_[rn/flat-list {:data       [{:title "test" :id 1} {:title "test3" :id 2}]
                     :renderItem item}]
    #_[rn/text {:style {:font-size 50}} "Hello Krell!!!"]])
 
 (defn ^:export -main [& args]
-  (r/as-element [hello]))
+  (r/as-element [main-page]))
---
+++ src/gpad/song_list.cljs
@@ -0,0 +1,34 @@
+(ns gpad.song-list
+  (:require [reagent.core :as r]
+            [reagent.react-native :as rn]
+            [react-native :as rrn]
+            [clojure.string :as str]))
+
+(defn separator []
+  [rn/view {:style {:borderBottomColor :gray
+                    :borderBottomWidth 1}}])
+
+(defn item [item]
+  (let [item (.-item item)
+        on-click (aget item "on-click")]
+    (js/console.log item)
+    (r/as-element
+      [rn/view
+       [rn/text
+        {:style   {:font-size     23 :margin-left 10 :margin-top 5
+                   :margin-bottom 5 :margin-right 10}
+         :onPress (fn [] (and on-click (on-click)))}
+        (.-title item)]
+       [separator]])))
+
+(defn song-list [data]
+  [rn/virtualized-list
+   {:renderItem         item
+    :data               []
+    :get-item           (fn [d index]
+                          (-> (get data index)
+                              (assoc :id (str index "song"))
+                              (clj->js)))
+    :get-item-count     (fn [d] (count data))
+    :key-extractor      (fn [item] (.-id item))
+    :initialNumToRender 11}])
